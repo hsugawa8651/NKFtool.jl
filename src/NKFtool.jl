b3630b7effd7952c9059cb5ec16b8f59f3ea5287
Hiroharu Sugawara <hsugawa@tmu.ac.jp>
2020-03-17 20:07:01 +0900

    dist: xenial

--- src/NKFtool.jl
+++ src/NKFtool.jl
@@ -1,16 +1,5 @@
-"""
-    NKFtool
 
-[Julia](https://julialang.org) package to guess and convert
-encodings of Japanese characters.
-This is a wrapper
-to Network kanji filter, aka [nkf](https://osdn.net/projects/nkf/),
-and provides the interface to use nkf command installed in your system from Julia.
-
-NKFtool requires nkf to be installed in your system.
-Major Unix-like operating systems offer its precompiled version
-in their package management systems.
-"""
+__precompile__(true)
 module NKFtool
 
 export nkf_version, nkf_help, nkf_guess, nkf_convert
